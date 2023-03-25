efe89763452fe3aaf51582f15c761390555ba6f6
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2023-03-25 18:59:31 +0530

    Update and rename pre-trained_DenseNet201.py to DenseNet201_featureExtrFunc.py

--- project code files/DenseNet201_featureExtrFunc.py
+++ project code files/DenseNet201_featureExtrFunc.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
 
 
 def load_model(num_outputs, inp_shape=(224, 224, 3)):
@@ -25,8 +26,14 @@ def load_model(num_outputs, inp_shape=(224, 224, 3)):
     new_model.add(Dropout(rate=0.6))
     new_model.add(BatchNormalization(momentum=0.9))
     new_model.add(Dense(units=num_outputs, activation='softmax'))
+    
+    # Compile the model
+    new_model.compile(loss="categorical_crossentropy",
+                      optimizer=SGD(learning_rate=0.001, momentum=0.9, nesterov=True),
+                      metrics=["accuracy"])
 
     return new_model
 
 
 m = load_model(num_outputs=170, inp_shape=(224, 224, 3))
+m.summary()
