c64760333fb3f9e6c906d6a3f5b5bbfd23f70e0c
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2023-04-04 22:33:13 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import os
 from PIL import Image
 import numpy as np
 import re
+import pickle
 
 from preprocessing_utils import preprocess
 from roi_extraction_utils import get_inner_hand_surface_ROI
@@ -16,8 +17,6 @@ def load_palmar_data(dirpath):
         # Read the content of the file
         image = Image.open(os.path.join(dirpath, fname))
         image = np.array(image)
-        # Preprocess the image
-        image = preprocess(image)
         # Extract ROI (palmprint)
         roi = get_inner_hand_surface_ROI(image)
         # Assign image-id as its label
@@ -25,8 +24,20 @@ def load_palmar_data(dirpath):
         label = int(label.group()) # label.dtype == 'int32'
 
         if roi is not None:
+            roi = preprocess(roi)
             rois.append(roi)
             labels.append(label)
     
     rois, labels = np.array(rois), np.array(labels)
     return rois, labels
+
+
+def save_model(model, filepath):
+    with open(filepath, "wb") as f:
+        pickle.dump(model, f)
+
+
+def load_model(filepath):
+    with open(filepath, "rb") as f:
+        model = pickle.load(f)
+    return model
