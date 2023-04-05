5e9f98b44affcb623b860606c9138ea307d4e587
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2023-04-05 18:31:11 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import re
 import pickle
 
 from preprocessing_utils import preprocess
-from roi_extraction_utils import get_inner_hand_surface_ROI
+from roi_extraction_utils import get_inner_hand_surface_ROI, get_landmark_ROI
 
 
 
@@ -32,11 +32,36 @@ def load_palmar_data(dirpath):
     return rois, labels
 
 
+
+def load_dorsal_data(dirpath, landmark):
+    rois, labels = [], []
+
+    for fname in os.listdir(dirpath):
+        # Read the content of the file
+        image = Image.open(os.path.join(dirpath, fname))
+        image = np.array(image)
+        # Extract landmark ROI
+        roi = get_landmark_ROI(image)
+        # Assign image-id as its label
+        label = re.search("[0-9]+", fname)
+        label = int(label.group()) # label.dtype == 'int32'
+
+        if roi is not None:
+            roi = preprocess(roi, gray_scale=False)
+            rois.append(roi)
+            labels.append(label)
+    
+    rois, labels = np.array(rois), np.array(labels)
+    return rois, labels
+
+
+
 def save_model(model, filepath):
     with open(filepath, "wb") as f:
         pickle.dump(model, f)
 
 
+        
 def load_model(filepath):
     with open(filepath, "rb") as f:
         model = pickle.load(f)
