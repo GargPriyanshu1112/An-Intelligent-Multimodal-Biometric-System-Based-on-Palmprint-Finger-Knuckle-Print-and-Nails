51a7e0909941b95fa3a44036b681dc41a1ff8c18
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2023-04-08 12:19:16 +0530

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -7,10 +7,15 @@ import cv2 as cv
 import os
 
 
+REQD_PALMAR_IMGS = 12
+REQD_DORSAL_IMGS = 12
+PERSONS_REQD = 165
+
+
 # Set up paths
-IMAGES_DIRPATH = "E:/college_project/11K_Hands2/"
+IMAGES_DIRPATH = "E:/college_project/11K_Hands3/"
 METADATA_FILEPATH = "E:/college_project/HandInfo.csv"
-DATASET_DIRPATH = "E:/college_project/college_dataset"
+DATASET_DIRPATH = "E:/college_project/college_dataset3"
 EXTRAS_FOLDER = os.path.join(DATASET_DIRPATH, "extra")
 PALMAR_DIRPATH = os.path.join(DATASET_DIRPATH, "palmar")
 PALMAR_TRAIN = os.path.join(PALMAR_DIRPATH, "train")
@@ -20,23 +25,42 @@ DORSAL_TRAIN = os.path.join(DORSAL_DIRPATH, "train")
 DORSAL_TEST  = os.path.join(DORSAL_DIRPATH, "test")
 
 
-REQD_PALMAR_IMGS = 12
-REQD_DORSAL_IMGS = 12
-PERSONS_REQD = 165
- 
 
-def save_files(ID, idx, files, dst_dirpath):
-    for fname in files:
-        os.replace(os.path.join(IMAGES_DIRPATH, fname),
-                   os.path.join(dst_dirpath, f"id-{ID}--{idx}.jpg"))
-                
-        if idx != 12:
-            idx += 1
-        else:
-            break        
-            
+def palmar_train_test_split(n_train):    
+    filenames = os.listdir(EXTRAS_FOLDER)
 
-def get_augmented_images_and_save_them(ID, num_reqd, filename, dst_dirpath):
+    # Save train images
+    train_fnames = random.sample(filenames, n_train)
+    for fname in train_fnames:
+        os.replace(os.path.join(EXTRAS_FOLDER, fname),
+                   os.path.join(os.path.join(PALMAR_TRAIN, fname)))
+        
+    # Save remaining images as test images
+    test_fnames = os.listdir(EXTRAS_FOLDER)
+    for fname in test_fnames:
+        os.replace(os.path.join(EXTRAS_FOLDER, fname),
+                   os.path.join(os.path.join(PALMAR_TEST, fname)))
+        
+
+
+def dorsal_train_test_split(n_train):    
+    filenames = os.listdir(EXTRAS_FOLDER)
+
+    # Save train images
+    train_fnames = random.sample(filenames, n_train)
+    for fname in train_fnames:
+        os.replace(os.path.join(EXTRAS_FOLDER, fname),
+                   os.path.join(os.path.join(DORSAL_TRAIN, fname)))
+        
+    # Save remaining images as test images
+    test_fnames = os.listdir(EXTRAS_FOLDER)
+    for fname in test_fnames:
+        os.replace(os.path.join(EXTRAS_FOLDER, fname),
+                   os.path.join(os.path.join(DORSAL_TEST, fname)))
+        
+        
+        
+def get_augmented_images_and_save_them(label, num_reqd, filename, dst_dirpath):
     # Read contents of the file
     image = cv.imread(os.path.join(IMAGES_DIRPATH, filename))
     image = tf.expand_dims(image, axis=0) # add `batch_size` dimension
@@ -49,51 +73,27 @@ def get_augmented_images_and_save_them(ID, num_reqd, filename, dst_dirpath):
     # Get required augmented images
     while(num_reqd > 0):
         aug_img = aug_iter.next()[0].astype('uint8')
-        aug_img_name = f"id-{ID}--{idx}_aug.jpg"
+        aug_img_name = f"label-{label}--{idx}_aug.jpg"
         cv.imwrite(os.path.join(dst_dirpath, aug_img_name), aug_img) # save
         
         num_reqd -= 1
         idx += 1
-    return idx
+    return idx        
 
 
-def palmar_train_test_split(ID, n_train):
-    os.mkdir(os.path.join(PALMAR_TRAIN, str(ID)))
-    os.mkdir(os.path.join(PALMAR_TEST, str(ID)))
-    
-    filenames = os.listdir(EXTRAS_FOLDER)
 
-    # Save train images
-    train_fnames = random.sample(filenames, n_train)
-    for fname in train_fnames:
-        os.replace(os.path.join(EXTRAS_FOLDER, fname),
-                   os.path.join(os.path.join(PALMAR_TRAIN, str(ID), fname)))
-        
-    # Save remaining images as test images
-    test_fnames = os.listdir(EXTRAS_FOLDER)
-    for fname in test_fnames:
-        os.replace(os.path.join(EXTRAS_FOLDER, fname),
-                   os.path.join(os.path.join(PALMAR_TEST, str(ID), fname)))
-        
+def save_files(label, idx, files, dst_dirpath):
+    for fname in files:
+        os.replace(os.path.join(IMAGES_DIRPATH, fname),
+                   os.path.join(dst_dirpath, f"label-{label}--{idx}.jpg"))
+                
+        if idx != 12:
+            idx += 1
+        else:
+            break  
 
-def dorsal_train_test_split(ID, n_train):
-    os.mkdir(os.path.join(DORSAL_TRAIN, str(ID)))
-    os.mkdir(os.path.join(DORSAL_TEST, str(ID)))
-    
-    filenames = os.listdir(EXTRAS_FOLDER)
 
-    # Save train images
-    train_fnames = random.sample(filenames, n_train)
-    for fname in train_fnames:
-        os.replace(os.path.join(EXTRAS_FOLDER, fname),
-                   os.path.join(os.path.join(DORSAL_TRAIN, str(ID), fname)))
-        
-    # Save remaining images as test images
-    test_fnames = os.listdir(EXTRAS_FOLDER)
-    for fname in test_fnames:
-        os.replace(os.path.join(EXTRAS_FOLDER, fname),
-                   os.path.join(os.path.join(DORSAL_TEST, str(ID), fname)))
-        
+
 
 # Read dataset metadata
 metadata = pd.read_csv(METADATA_FILEPATH)
@@ -101,10 +101,9 @@ metadata = pd.read_csv(METADATA_FILEPATH)
 # No. of individuals (each id denotes an individual)
 unique_ids = metadata.id.unique() 
 
-
 # Loop through each id (person). If number of palmar/dorsal images is less than
 # the required number, add augmented palmar/dorsal images in the dataset.
-person_num = 0
+label = 0
 for ID in unique_ids[2:]:
     palmar_fnames_list = metadata[(metadata.id == ID) & (metadata.aspectOfHand == 'palmar right')].imageName.values.tolist()
     dorsal_fnames_list = metadata[(metadata.id == ID) & (metadata.aspectOfHand == 'dorsal right')].imageName.values.tolist()
@@ -119,24 +118,22 @@ for ID in unique_ids[2:]:
             # Select a random imagefile and get its augmentations
             rand_fname = random.choice(palmar_fnames_list)
             
-            idx = get_augmented_images_and_save_them(ID,
+            idx = get_augmented_images_and_save_them(label,
                                                      num_reqd = REQD_PALMAR_IMGS - num_palmar_imgs,
                                                      filename=rand_fname, 
                                                      dst_dirpath=EXTRAS_FOLDER)
             
             # Save original imagefiles
-            save_files(ID=ID, idx=idx, files=palmar_fnames_list, dst_dirpath=EXTRAS_FOLDER)
+            save_files(label=label, idx=idx, files=palmar_fnames_list, dst_dirpath=EXTRAS_FOLDER)
         
         else:
             # Save required number of imagefiles
-            save_files(ID=ID, idx=1, files=palmar_fnames_list, dst_dirpath=EXTRAS_FOLDER)
+            save_files(label=label, idx=1, files=palmar_fnames_list, dst_dirpath=EXTRAS_FOLDER)
 
-        palmar_train_test_split(ID, n_train=8)
+        palmar_train_test_split(n_train=8)
             
         # ****************************************************************************************** #            
             
-
-
 
 
         # *****************************   FOR DORSAL IMAGES  *************************************** #
@@ -145,25 +142,25 @@ for ID in unique_ids[2:]:
             # Select a random imagefile and get its augmentations
             rand_fname = random.choice(dorsal_fnames_list)
             
-            idx = get_augmented_images_and_save_them(ID,
+            idx = get_augmented_images_and_save_them(label,
                                                      num_reqd = REQD_DORSAL_IMGS - num_dorsal_imgs,
                                                      filename=rand_fname, 
                                                      dst_dirpath=EXTRAS_FOLDER)
             
             # Save original imagefiles
-            save_files(ID=ID, idx=idx, files=dorsal_fnames_list, dst_dirpath=EXTRAS_FOLDER)
+            save_files(label, idx=idx, files=dorsal_fnames_list, dst_dirpath=EXTRAS_FOLDER)
             
         else:
             # Save required original imagefiles
-            save_files(ID=ID, idx=1, files=dorsal_fnames_list, dst_dirpath=EXTRAS_FOLDER)
+            save_files(label, idx=1, files=dorsal_fnames_list, dst_dirpath=EXTRAS_FOLDER)
         
-        dorsal_train_test_split(ID, n_train=8)
+        dorsal_train_test_split(n_train=8)
         
         # ****************************************************************************************** #
         
-        person_num += 1
+        label += 1
         
         # If biometrics of required number of individuals is collected,
         # end the dataset preparation process
-        if person_num == PERSONS_REQD:
+        if label == PERSONS_REQD:
             break
